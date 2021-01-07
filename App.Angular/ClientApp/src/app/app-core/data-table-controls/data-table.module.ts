dd7f3a4ec156968f88f91e59604f636e7a7307b0
Nam Le Thanh <lthanhnam@tma.com.vn>
2021-01-07 11:39:38 +0700

    Allow export data table using mat-table-exporter

--- App.Angular/ClientApp/src/app/app-core/data-table-controls/data-table.module.ts
+++ App.Angular/ClientApp/src/app/app-core/data-table-controls/data-table.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedMaterialModule } from '../../app.shared.materials.modules';
 import { DataTableComponent } from './components/data-table.component';
+import { MatTableExporterModule } from 'mat-table-exporter';
 
 @NgModule({
     imports: [CommonModule, FormsModule,
-        SharedMaterialModule
+        SharedMaterialModule,
+        MatTableExporterModule
     ],
     declarations: [DataTableComponent],
     exports: [DataTableComponent]
